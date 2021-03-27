bedd1fcbebe3fe9a3811d318384ff2cb1ba31d0b
HoraceSupertramp <horace.supertramp@gmail.com>
2021-03-27 13:49:57 +0100

    adds react-based test

--- src/components/App/index.spec.tsx
+++ src/components/App/index.spec.tsx
@@ -1,11 +1,18 @@
 import { expect } from 'chai';
-import { container } from './index.css';
+import {render} from "react-dom";
+import App from "./index";
+import React from "react";
 
 describe('Test Unit', () => {
+
+    const div = document.createElement('div');
+    div.id = 'root';
+
+    before(() => document.body.appendChild(div))
+
     it('works', () => {
-        const div = document.createElement('div');
-        div.className = container;
-        document.body.appendChild(div);
-        expect(window.getComputedStyle(div).getPropertyValue('height')).to.equal('50px');
+        render(<App/>, div);
+        expect(window.getComputedStyle(document.querySelector('#root > div')!).getPropertyValue('height')).to.equal('50px');
     });
+
 });
